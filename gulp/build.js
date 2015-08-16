4e149c16fe670aa4e8fff3fa643ab24d8c82843f
Inaki Anduaga <inaki.anduaga@tado.com>
2015-08-16 13:33:16 +0200

    #5: Added swagger build tasks

--- gulp/build.js
+++ gulp/build.js
@@ -3,15 +3,15 @@
 //Dependencies
 var gulp = require('gulp'),
   $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'del', 'run-sequence']
+    pattern: ['gulp-*', 'del', 'run-sequence', 'js-yaml']
   }),
   config = require('./config.json'),
   tsConfig = require('../tsconfig.json');
-      
+
 
 /**
  * Create typescript project build reference for incremental compilation under watch tasks
- * 
+ *
  * @link https://github.com/ivogabe/gulp-typescript
  */
 var tsProject = $.typescript.createProject('tsconfig.json', {
@@ -29,9 +29,27 @@ gulp.task('clean', false, function () {
 /**
  * Precopies all non-ts files into the dist folder
  */
-gulp.task('copyNonTs', false, function () {
+gulp.task('copyNonTs', false, [], function () {
   return gulp.src(['src/.env','src/**/*', '!src/**/*.ts'])
     .pipe(gulp.dest('dist'));
+});
+
+/**
+ * Precopies the swagger-ui assets into the dist folder
+ */
+gulp.task('copySwaggerUI', false, [], function () {
+  return gulp.src(['node_modules/swaggerize-ui/node_modules/swagger-ui/dist/**/*'])
+    .pipe(gulp.dest('dist/swagger/dist'));
+});
+
+/**
+ * Converts swagger spec from json to yaml format
+ */
+gulp.task('convertSwaggerSpecFromYamlToJson', false, [], function () {
+  var fs = require('fs');
+  var swaggerSpec = $.jsYaml.safeLoad(fs.readFileSync('./src/swagger/spec.yaml', 'utf8'));
+  fs.writeFileSync('dist/swagger/spec.json', JSON.stringify(swaggerSpec));
+  return;
 });
 
 /**
@@ -42,15 +60,15 @@ gulp.task('lint', 'Runs a typescript linter on the application code', function (
   return gulp.src(config.tsLinter.sources)
     .pipe($.tslint(config.tsLinter.options))
     .pipe($.tslint.report(config.tsLinter.reporter));
-    
+
 });
 
 /**
  * Compiles typescript app into js
  */
 gulp.task('compile', false, function () {
-  
-  var tsResult = gulp.src(tsConfig.files)  
+
+  var tsResult = gulp.src(tsConfig.files)
       .pipe($.typescript(tsProject, undefined, $.typescript.reporter.longReporter()));
 
   return tsResult.js
@@ -60,9 +78,11 @@ gulp.task('compile', false, function () {
 /**
  * Build the server app
  */
-gulp.task('build', 'Builds the server app (compiles & copies)', function (callback) {  
+gulp.task('build', 'Builds the server app (compiles & copies)', function (callback) {
   return $.runSequence('clean',
-              ['compile'],
-              'copyNonTs',
-              callback);
+    ['compile'],
+    'copyNonTs',
+    'copySwaggerUI',
+    'convertSwaggerSpecFromYamlToJson',
+    callback);
 });
--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -47,7 +46,7 @@ app.use(settingsRoutes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     res.status(404);
     console.log('catching 404 error');
     return next(err);
